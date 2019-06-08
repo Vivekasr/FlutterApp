945381adba7a5c830f3d4f36ce17f93eb132ce50
Vivekasr <hie.vivi.sharma@gmail.com>
2019-06-08 15:10:56 +0530

    FirstAdd

---
+++ image_share_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ImageShareApp());
+
+class ImageShareApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
